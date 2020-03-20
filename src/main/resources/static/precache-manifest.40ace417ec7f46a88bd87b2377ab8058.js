1ccad77e3a088e67125e695e32d3b21f4fc9708c
Yue Sun <ysun276@wisc.edu>
2020-03-20 01:22:05 -0400

    try

---
+++ src/main/resources/static/precache-manifest.40ace417ec7f46a88bd87b2377ab8058.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c66f39cc2225b9542a5e37df8d72f73c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32179e4c0bcb00a1e9d5",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "576d3da560941f79ec9c",
+    "url": "/static/js/2.a5de995a.chunk.js"
+  },
+  {
+    "revision": "1ae01fb0d60dfdefe7f9d6cbefaaa517",
+    "url": "/static/js/2.a5de995a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32179e4c0bcb00a1e9d5",
+    "url": "/static/js/main.78645fb9.chunk.js"
+  },
+  {
+    "revision": "165f034ffb316832c97f",
+    "url": "/static/js/runtime-main.86e74220.js"
+  }
+]);
